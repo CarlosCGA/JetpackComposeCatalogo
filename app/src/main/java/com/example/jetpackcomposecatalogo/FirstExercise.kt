04b6f8586be1f1b1f32171270a2c41d96d8afe57
Carlos <ccaieg00@estudiantes.unileon.es>
2024-03-14 23:37:54 +0100

    add FirstExercise done

---
+++ app/src/main/java/com/example/jetpackcomposecatalogo/FirstExercise.kt
@@ -0,0 +1,68 @@
+package com.example.jetpackcomposecatalogo
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun FirstExercise() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1F)
+                .background(Color.Cyan), contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Ejemplo 1")
+        }
+
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .weight(1F)) {
+            Box(
+                modifier = Modifier
+                    .weight(1F)
+                    .background(Color.Red)
+                    .fillMaxHeight(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Ejemplo 2")
+            }
+
+            Box(
+                modifier = Modifier
+                    .weight(1F)
+                    .background(Color.Green)
+                    .fillMaxHeight(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Ejemplo 3")
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1F)
+                .background(Color.Magenta), contentAlignment = Alignment.BottomCenter
+        ) {
+            Text(text = "Ejemplo 4")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewFirstExercise() {
+    FirstExercise()
+}
