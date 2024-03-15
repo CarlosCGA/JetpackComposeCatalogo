ea5308e4b26766824db09ef25a1e9c5bf81f31fe
Carlos <ccaieg00@estudiantes.unileon.es>
2024-03-15 11:57:34 +0100

    add first constraintLayout composable add SecondExercise

---
+++ app/src/main/java/com/example/jetpackcomposecatalogo/exercises/SecondExercise.kt
@@ -0,0 +1,73 @@
+package com.example.jetpackcomposecatalogo.exercises
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+
+@Composable
+fun MyConstraintLayout() {
+
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+        val (boxRed, boxBlue, boxYellow, boxMagenta, boxGreen) = createRefs()
+
+        Box(modifier = Modifier
+            .size(125.dp)
+            .background(Color.Red)
+            .constrainAs(boxRed) {
+                start.linkTo(parent.start)
+                top.linkTo(parent.top)
+                end.linkTo(parent.end)
+                bottom.linkTo(parent.bottom)
+            })
+        Box(
+            modifier = Modifier
+                .size(125.dp)
+                .background(Color.Blue)
+                .constrainAs(boxBlue) {
+                    top.linkTo(boxRed.bottom)
+                    start.linkTo(boxRed.end)
+                }
+        )
+        Box(
+            modifier = Modifier
+                .size(125.dp)
+                .background(Color.Yellow)
+                .constrainAs(boxYellow) {
+                    end.linkTo(boxRed.start)
+                    bottom.linkTo(boxRed.top)
+                }
+        )
+        Box(
+            modifier = Modifier
+                .size(125.dp)
+                .background(Color.Magenta)
+                .constrainAs(boxMagenta) {
+                    bottom.linkTo(boxRed.top)
+                    start.linkTo(boxRed.end)
+                }
+        )
+        Box(
+            modifier = Modifier
+                .size(125.dp)
+                .background(Color.Green)
+                .constrainAs(boxGreen) {
+                    top.linkTo(boxRed.bottom)
+                    end.linkTo(boxRed.start)
+                }
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewMyConstraintLayout() {
+    MyConstraintLayout()
+}
