2979953f0d6789ff144b01fe20a888d93a7901b1
Carlos <carlos.canete@espaciosoa.com>
2024-03-16 16:28:49 +0100

    update MyTextField

--- app/src/main/java/com/example/jetpackcomposecatalogo/composables/MyTextField.kt
+++ app/src/main/java/com/example/jetpackcomposecatalogo/composables/MyTextField.kt
@@ -3,6 +3,7 @@ package com.example.jetpackcomposecatalogo.composables
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -15,9 +16,13 @@ import androidx.compose.ui.tooling.preview.Preview
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MyTextField() {
+    var inputText by remember { mutableStateOf(""/*"Default value (mainly empty)"*/) }
+
     Column(Modifier.fillMaxSize()) {
-        var inputText by remember { mutableStateOf("Default value") }
-        TextField(value =inputText, onValueChange ={ newText -> inputText = newText})
+        TextField(
+            value = inputText, onValueChange = { newText -> inputText = newText },
+            label = { Text(text = "Escribe aqui lo que te sugiera la vida")}
+        )
     }
 }
 
