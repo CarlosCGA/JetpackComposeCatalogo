0c704bc3ea9e8f84e9f32e4a22e25fdc1f1a1565
Carlos <ccaieg00@estudiantes.unileon.es>
2024-03-18 16:58:06 +0100

    add state variable to MyButton composable

--- app/src/main/java/com/example/jetpackcomposecatalogo/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposecatalogo/MainActivity.kt
@@ -25,7 +25,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -118,7 +117,7 @@ class MainActivity : ComponentActivity() {
                         MyOutlinedTextField(favoriteColor, { newText -> favoriteColor = newText}, "Favorite color")
 
                         MyDivider(title = "MyButton")
-                        MyButton(applicationContext)
+                        MyButton()
                     }
                 }
             }
@@ -219,7 +218,7 @@ fun PreviewMain() {
                 MyOutlinedTextField(favoriteColor, { newText -> favoriteColor = newText}, "Favorite color")
 
                 MyDivider(title = "MyButton")
-                MyButton(LocalContext.current)
+                MyButton()
             }
         }
     }
--- app/src/main/java/com/example/jetpackcomposecatalogo/composables/MyButton.kt
+++ app/src/main/java/com/example/jetpackcomposecatalogo/composables/MyButton.kt
@@ -1,22 +1,34 @@
 package com.example.jetpackcomposecatalogo.composables
 
-import android.content.Context
-import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun MyButton(context: Context) {
-    Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
-        Button(onClick = { Toast.makeText(context, "Button clicked!", Toast.LENGTH_SHORT).show() }) {
+fun MyButton() {
+    var isEnabled by rememberSaveable {
+        mutableStateOf(true)
+    }
+    Column(
+        Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = {
+            isEnabled = false
+        },
+            enabled = isEnabled
+        ) {
             Text(text = "Click me!")
         }
     }
@@ -26,5 +38,5 @@ fun MyButton(context: Context) {
 @Preview(showBackground = true)
 @Composable
 fun PreviewMyButton() {
-    MyButton(context = LocalContext.current)
+    MyButton()
 }
