8ad8b000292e59c4a36b8e58b9c326d9d8ab8d48
Carlos <ccaieg00@estudiantes.unileon.es>
2024-03-18 17:21:51 +0100

    update Image to circle shape

--- app/src/main/java/com/example/jetpackcomposecatalogo/composables/MyImage.kt
+++ app/src/main/java/com/example/jetpackcomposecatalogo/composables/MyImage.kt
@@ -1,14 +1,19 @@
 package com.example.jetpackcomposecatalogo.composables
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposecatalogo.R
 
 @Composable
@@ -16,7 +21,8 @@ fun MyImage() {
     Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
         Image(
             painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "Android icon"
+            contentDescription = "Android icon",
+            modifier = Modifier.clip(CircleShape).border(5.dp, Color.Red, CircleShape)
         )
     }
 }
